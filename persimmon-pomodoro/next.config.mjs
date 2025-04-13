ad5d07834dd7a23b4146a0217a6447da808c5337
sylviapan1852 <85336022+sylviapan1852@users.noreply.github.com>
2025-04-13 15:48:59 +1000

    changes to host repo publicly

--- persimmon-pomodoro/next.config.mjs
+++ persimmon-pomodoro/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isGithubPages = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  basePath: isGithubPages ? '/persimmon-pomodoro' : '',
+  assetPrefix: isGithubPages ? '/persimmon-pomodoro/' : '',
+};
 
 export default nextConfig;
