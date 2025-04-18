9723037c9ca7d1a042bc2ffc2eea5cad60076394
Sylvia Pan <sylvia.pan1852@gmail.com>
2025-04-18 18:36:48 +1000

    debugging preload

--- persimmon-pomodoro/next.config.mjs
+++ persimmon-pomodoro/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  basePath: '/persimmon-pomodoro',
+  assetPrefix: '/persimmon-pomodoro/',
+  trailingSlash: true, // ensures paths end in / which helps with static hosting
+  output: 'export',     // if you're using static export (for GitHub Pages)
 };
 
 export default nextConfig;
